eb4f6aef5886bdce01ebd8d56bc607e30b8ff9e5
Mihai DEACONU <mihai.deaconu@cognizant.com>
2025-05-22 18:20:23 +0300

    Displaying Meals
    Signed-off-by: Mihai DEACONU <mihai.deaconu@cognizant.com>

--- screens/MealsOverviewScreen.jsx
+++ screens/MealsOverviewScreen.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import { FlatList, StyleSheet, View } from "react-native";
 import PropTypes from "prop-types";
 
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
 const MealsOverviewScreen = ({ route }) => {
   const categoryId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((item) => {
+    return item.categoryIds.indexOf(categoryId) >= 0;
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(itemData) => itemData.id}
+        renderItem={(itemData) => <MealItem title={itemData.item.title} />}
+      />
     </View>
   );
 };
