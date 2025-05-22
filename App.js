07c4b9a6caa21b075085032b7ac788794d59dd66
Mihai DEACONU <mihai.deaconu@cognizant.com>
2025-05-22 17:41:27 +0300

    Clean-up App.js
    Signed-off-by: Mihai DEACONU <mihai.deaconu@cognizant.com>

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
